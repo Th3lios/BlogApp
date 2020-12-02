f8c0e50d81b5f0496c51010b81917bd13c8de500
elias <earayad@hotmail.cl>
2020-12-02 11:12:49 -0300

    looping card in project done

--- src/screens/ProjectScreen/ProjectScreen.js
+++ src/screens/ProjectScreen/ProjectScreen.js
@@ -3,22 +3,88 @@ import {View, StyleSheet, Animated, PanResponder} from 'react-native';
 import Project from '../../components/Projects/Project';
 
 const ProjectScreen = () => {
-  const [pan, setPan] = useState(new Animated.ValueXY());
+  const [pan] = useState(new Animated.ValueXY());
+  const [scale] = useState(new Animated.Value(0.9));
+  const [translateY] = useState(new Animated.Value(44));
+  const [thirdScale] = useState(new Animated.Value(0.8));
+  const [thirdTranslateY] = useState(new Animated.Value(-50));
   const panResponder = useRef(
     PanResponder.create({
       onMoveShouldSetPanResponder: (evt, gestureState) => true,
       onPanResponderMove: Animated.event([null, {dx: pan.x, dy: pan.y}]),
-      onPanResponderRelease: () => {
-        Animated.spring(pan, {
-          toValue: {x: 0, y: 0},
+      onPanResponderGrant: () => {
+        Animated.spring(scale, {
+          toValue: 1,
           useNativeDriver: false,
         }).start();
+        Animated.spring(translateY, {
+          toValue: 0,
+          useNativeDriver: false,
+        }).start();
+        Animated.spring(thirdScale, {
+          toValue: 0.9,
+          useNativeDriver: false,
+        }).start();
+        Animated.spring(thirdTranslateY, {
+          toValue: 44,
+          useNativeDriver: false,
+        }).start();
+      },
+      onPanResponderRelease: () => {
+        const positionY = pan.y.__getValue();
+        if (positionY > 200) {
+          Animated.timing(pan, {
+            toValue: {x: 0, y: 1000},
+            useNativeDriver: false,
+          }).start(() => {
+            pan.setValue({x: 0, y: 0});
+            scale.setValue(0.9);
+            translateY.setValue(44);
+            thirdScale.setValue(0.8);
+            thirdTranslateY.setValue(-50);
+          });
+        } else {
+          Animated.spring(pan, {
+            toValue: {x: 0, y: 0},
+            useNativeDriver: false,
+          }).start();
+          Animated.spring(scale, {
+            toValue: 0.9,
+            useNativeDriver: false,
+          }).start();
+          Animated.spring(translateY, {
+            toValue: 44,
+            useNativeDriver: false,
+          }).start();
+          Animated.spring(thirdScale, {
+            toValue: 0.8,
+            useNativeDriver: false,
+          }).start();
+          Animated.spring(thirdTranslateY, {
+            toValue: -50,
+            useNativeDriver: false,
+          }).start();
+        }
       },
     }),
   ).current;
 
   return (
     <View style={styles.container}>
+      <Animated.View
+        style={[
+          styles.project,
+          {transform: [{scale: thirdScale}, {translateY: thirdTranslateY}]},
+        ]}>
+        <Project />
+      </Animated.View>
+      <Animated.View
+        style={[
+          styles.project,
+          {transform: [{scale: scale}, {translateY: translateY}]},
+        ]}>
+        <Project />
+      </Animated.View>
       <Animated.View
         style={{
           transform: [{translateX: pan.x}, {translateY: pan.y}],
@@ -35,6 +101,16 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  project: {
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex: -1,
   },
 });
 
